89c2fb2177c56508ccf75e7fc2bbba4e1eb5a37e
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-10-03 12:30:50 +0530

    adding dynamic routes for users and sub document for genre preference of users

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ mongoose.connect('mongodb://localhost:27017/usersDB',{useNewUrlParser:true,useUn
 
 // Schema
 
+// const genreSchema=new mongoose.Schema({name:String})
+
 const userSchema=new mongoose.Schema({
 	username:String,
 	password:String,
 	gender:String,
+	// genre:[genreSchema]
 	genre:[{name:String}]
 })
 
@@ -42,7 +45,9 @@ app.post('/login',function(req,res){
 			res.send(err)
 		else{
 			if(user.password===password){
-				res.send(`Welcome ${user}`)
+				// res.send(`Welcome ${user}`)
+				// res.redirect('/match/:user')
+				res.redirect('/match/'+username);
 			}
 			else
 				res.send('Password is incorrect')
@@ -73,12 +78,18 @@ app.post('/signup',function(req,res){
 	const user=new User({
 		username:req.body.username,
 		password:req.body.password,
-		gender:req.body.gender,
+		gender:req.body.gender
 
-		genre.push(req.body.genre)
+		// genre.push(req.body.genre)
 	})
+	user.genre.push(req.body.genre)
 	user.save();
 })
+
+app.get('/match/:user'){
+	console.log(req.params);
+
+}
 
 
 app.listen(port,function(){
